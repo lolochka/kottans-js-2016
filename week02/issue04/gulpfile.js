ab89eec77c45958c81778dade47d607572591e6d
lolochka <ooshkoda@gmail.com>
2016-06-01 06:57:08 +0300

    Gulp added

---
+++ week02/issue04/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync');
+    postcss = require('gulp-postcss'),
+    cssnext = require('cssnext');
+
+var config = {
+    server: {
+        baseDir: "./"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+gulp.task('css:build', function () {
+    var processors = [
+        cssnext()
+    ];
+    return gulp.src('./src/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('build',['css:build']);
+
+gulp.task('watch', function() {
+    watch(['./src/*.css'], function(event,cb) {
+        gulp.start('css:build');
+    });
+})
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
